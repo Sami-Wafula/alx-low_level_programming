ae991a686e1aba00499db1a3b83309a27b09e2ae
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-18 03:38:45 -0700

    Revert "print numbers"; man has erred
    This reverts commit 67100fdba51e208de105d940140ff14a22037bb0.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int x;
 
-	for (x = 48; x < 58; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 		_putchar(x);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
