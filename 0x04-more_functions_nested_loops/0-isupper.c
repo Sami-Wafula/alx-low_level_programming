969946bab9c13defd63f4bb97551e578bfb6604a
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-17 21:55:13 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper: check if letter is upper case
+ * @c: c is an ascii character
+ * Return: Always 0 if true, 1 if false
+ */
+int main(void)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+
+
+
