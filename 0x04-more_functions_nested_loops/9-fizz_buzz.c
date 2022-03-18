907da07712382ab80ec0428441e2e929192d4d02
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-18 02:37:39 -0700

    fizz? buzz? fizzbuzz!

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,11 @@ int main(void)
 	for (1 <= 100; i++;)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
 			printf("%d ", i);
 	}
