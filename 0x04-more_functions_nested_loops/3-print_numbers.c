db39e4804b48a74d2ed7c28b95a849a435e2d19c
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-18 03:05:50 -0700

    prints numbers(0-9)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
