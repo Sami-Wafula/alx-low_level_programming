32821470e8842a46b863cbd730c5254a741a0212
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-18 01:06:10 -0700

    mul- returns the product of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: 1
+ */
+
+int mul(int a, int b)
+{
+	mul = a * b;
+	printf("%d/n", mul(int a, int b);
+	return (0);
+}
