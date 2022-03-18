be35a736a5e2e48b7908ce2843053b35785c54da
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-18 00:31:10 -0700

    isdigit- checks if given input is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for alphabetic character
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if digit)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
