10491341074524abf4a13f7378ee92090ecbd139
Sami-Wafula <samiwafula3257@gmail.com>
2022-03-21 13:33:44 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/** 
+ * main - update pointer value
+ *
+ * Result: Always 0.
+ */
+int main(void)
+{
+	int m = 98;
+	int *p = &m;
+	printf("m=%d\n):
+	return (0);
+}
