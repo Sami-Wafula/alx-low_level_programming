751632d961ba6ca1d95b0d29702f11cb30f21669
Sami-Wafula <samiwafula3257@gmail.com>
2022-04-01 12:02:58 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints it's name
  * @argc: number of arguments
@@ -7,7 +7,7 @@
  *
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
