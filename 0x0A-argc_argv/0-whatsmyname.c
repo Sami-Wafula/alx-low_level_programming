8fc0356359b2627cf4002302e4e03e36d94ac578
Sami-Wafula <samiwafula3257@gmail.com>
2022-04-01 07:26:18 -0700

    header and first function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main: prints it's name
+ * @argc - number of arguments
+ * @argv - array of all argument names
+ *
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+  int count;
+
+  printf ("%s\n",argv[0]);
+
+ return 0;
+}
