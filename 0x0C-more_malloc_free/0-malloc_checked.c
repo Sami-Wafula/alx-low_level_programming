460b0394835d4f493caae367d3669291d476e27b
Sami-Wafula <samiwafula3257@gmail.com>
2022-04-07 12:39:31 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *malloc_checked - memory allocation
+ * @b: int
+ * Return: 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	str = malloc(sizeof(int) * b);
+	printf("%\n", str);
+	return (0);
+}
