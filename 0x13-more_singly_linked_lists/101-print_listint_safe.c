ab641a27231657458a9db3957a098186df2b22c9
Sami-Wafula <samiwafula3257@gmail.com>
2022-04-26 13:30:49 -0700

    Modify

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,59 +1,37 @@
+#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * _r - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
+ * print_listint_safe - prints a singly linked list (can print lists with loop)
+ * @head: head pointer of singly linked list
  *
- * Return: pointer to the new list
- */
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
-{
-	const listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
-
-/**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list
- *
- * Return: the number of nodes in the list
+ * Return: Number of elements in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, num = 0;
-	const listint_t **list = NULL;
+	const listint_t *tmp, *tmp2;
+	unsigned int count = 0;
 
-	while (head != NULL)
+	tmp = head;
+	if (tmp == 0)
+		return (0);
+
+	while (tmp != 0)
 	{
-		for (i = 0; i < num; i++)
+		tmp2 = tmp;
+		tmp = tmp->next;
+
+		count++;
+
+		printf("[%p] %d\n", (void *)tmp2, tmp2->n);
+
+		if (tmp2 <= tmp)
 		{
-			if (head == list[i])
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (num);
-			}
+			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+			break;
 		}
-		num++;
-		list = _r(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
 	}
-	free(list);
-	return (num);
+
+	return (count);
 }
